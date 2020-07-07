cfac55333bc5a04ef9e95782d45a02449a074108
SAI-HOME <mesoftkor@gmail.com>
2020-07-07 23:45:17 +0900

    테스트 커밋2

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 
         return (
             <div>
-                <h1>Hello React Skeleton!!!!!!</h1>
+                <h1>Hello React Skeleton!!!!!!!!!!!!!</h1>
                 <h2>{this.state.value}</h2>
                 <button onClick={() => { this.setState({value: this.state.value + 1}) }}>CLICK ME</button>
             </div>
